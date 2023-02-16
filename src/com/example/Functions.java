425008fc9dcf21b96d4a5080301c59e7b809a21f
dialaay5 <diala.ay5@gmail.com>
2023-02-16 02:13:59 +0200

    upload all

---
+++ src/com/example/Functions.java
@@ -0,0 +1,200 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Functions {
+
+    static String magicWord = "java";
+
+    public static void greet(){ //"Question 3"
+        System.out.println("good morning!!");
+    }
+
+    public static void printNum(int num){ //"Question 4"
+        System.out.println(num);
+    }
+
+    //"Question 5 in class Main"
+
+    public static int changeNumber(int number){ //"Question 6"
+        return number + 1;
+    }
+
+    public static int multiply(int x, int y){ //"Question 7"
+        return x * y;
+    }
+
+    public static void calc(int a, int b , char operator){ //"Question 8"
+        if(operator == '+'){
+            System.out.printf("a + b = %d", a + b);
+        }
+        else if(operator == '-'){
+            System.out.printf("a - b = %d", a - b);
+        }
+    }
+
+    public static int average(int x, int y, int z){ //"Question 9"
+        int sum = x + y + z;
+        return sum / 3 ;
+    }
+
+    public static boolean inArray(int number, int []arr){ //"Question 10"
+        boolean inTheArray = false;
+        for (int j : arr) {
+            if (j == number) {
+                inTheArray = true;
+
+                break;
+            }
+        }
+        return inTheArray;
+    }
+
+    public static void divide(int x){ //"Question 11"
+        int i = 1;
+        while (i <= x){
+            if(x % i == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+
+    public static void hello(){ //"Question 12"
+        System.out.printf("hello %s\n", world());
+    }
+    public static String world(){
+        return "world";
+    }
+
+    //"Question 13 in class Main"
+
+    public static void numberType(int x, int y) { //"Question 14"
+        System.out.println("int");
+    }
+    public static void numberType(double x, double y) {
+        System.out.println("double");
+    }
+    public static void numberType(float x, float y) {
+        System.out.println("float");
+    }
+
+    public static void getScanner() { //"Question 15"
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = s.next();
+        System.out.println(name + " is a nice name!");
+    }
+
+    public static int randomize(int []arr1){ //"Question 16"
+        // a random integer from 0 to (arr1.length) where arr1.length is not included.:
+        int i = (int) Math.floor(Math.random() * arr1.length);
+        return (arr1[i]);
+        }
+
+    public static void strings(String string){ //"Question 17"
+        System.out.printf((string +"@@@").concat(star()).concat(sulamet()));
+    }
+    public static String star(){
+        return "**";
+    }
+    public static String sulamet(){
+        return "#";
+    }
+
+    //"Question 18 in class Main"
+
+    public static void printWord(){ //"Question 19"
+        System.out.println(magicWord);
+    }
+    public static void changeWord(String word){
+         magicWord = word;
+        System.out.println(magicWord);
+    }
+
+    public static void multiplyZugi(int []arr2){ //"Question 20"
+        for (int i = 0; i < arr2.length; i++) {
+            if(arr2[i] % 2 == 0){
+                arr2[i] = arr2[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
+    public static void addOne(int []arr2){
+        arr2[arr2.length - 1] = arr2[arr2.length - 1] + 1;
+        System.out.println(Arrays.toString(arr2));
+    }
+    public static void setZero(int []arr2){
+        for (int i = 0; i < arr2.length; i++) {
+            if(arr2[i] % 2 != 0){
+                arr2[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
+    public static int sumOfDigits(int number) { //"Question 21"
+        int sum = 0;
+        while(number > 0){
+            sum = sum + (number % 10);
+            number = (int) Math.floor(number / 10);
+        }
+        return sum;
+        }
+
+    public static boolean password(String[] passwordArray) { //"Question 22"
+        boolean correctPassword = false;
+
+        for (String j : passwordArray) {
+                if (Character.isLetterOrDigit(j.charAt(0)) && passwordArray.length >= 8) {
+                    correctPassword = true;
+                }
+                else{
+                    correctPassword = false;
+                    break;
+                }
+            }
+            return correctPassword;
+        }
+
+        public static void matrix1(){ //"Question 23"
+            int rows = 3;
+            int columns = 3;
+            int[][] matrixArray = new int[rows][columns];
+
+            Random rand = new Random();
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrixArray[i][j] = rand.nextInt(100) + 1;
+                }
+            }
+            System.out.println(Arrays.deepToString(matrixArray));
+            sumMatrix1(matrixArray);
+     }
+
+    public static void sumMatrix1(int[][] array2d){
+        int sum = 0;
+        for (int[] array : array2d) {
+            for (int element : array) {
+                sum += element;
+            }
+        }
+            System.out.println(sum);
+    }
+
+    //"Question 24 in class Main"
+
+    public static int atsertRecursion(int index) { //"Question 25"
+        if (index <= 1) {
+            return 1;
+        }
+        return index * atsertRecursion((index - 1));
+    }
+
+
+
+    }
+
+
+
---
+++ src/com/example/Main.java
@@ -0,0 +1,141 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Question 1");
+        System.out.println("What is a function? What is it good for?\n "+
+                "A function is a block of code which only runs when it is called and used to perform certain actions.\n" +
+                "The input of those functions called “parameters”, we can pass as many parameters as we \n" +
+                "want..\n"+
+                " By using functions we can:- \n"+
+                " Use the same block of code everywhere we want without rewrite it. \n"+
+                " Run code only when something that we want happen (like pressing a button).\n"+
+                " Keep our code short and clean.\n"+
+                " Better explain our code to other developers.\n");
+
+        System.out.println("Question 2");
+        System.out.println("What is the name of the first function called in the program?\n "+ "main.\n");
+
+        System.out.println("Question 3");
+        Functions.greet();
+
+        System.out.println("\nQuestion 4");
+        Functions.printNum(56);
+
+        System.out.println("\nQuestion 5");
+        int i = 1;
+        while (i <= 10){
+            Functions.printNum(i);
+            i++;
+        }
+
+        System.out.println("\nQuestion 6");
+        int k = 5;
+        System.out.println(Functions.changeNumber(k)); // k = 6
+
+        System.out.println("\nQuestion 7");
+        System.out.println(Functions.multiply(5,4));
+        System.out.println(Functions.multiply(3,6));
+        System.out.println(Functions.multiply(7,5));
+
+        System.out.println("\nQuestion 8");
+        Functions.calc(2, 10, '-');
+
+        System.out.println("\nQuestion 9");
+        System.out.println(Functions.average(5,4, 0));
+
+        System.out.println("\nQuestion 10");
+        int[] arr = {1, 2, 5, 10};
+        System.out.println(Functions.inArray(100, arr));
+
+        System.out.println("\nQuestion 11");
+        Functions.divide(15);
+
+        System.out.println("\nQuestion 12");
+        Functions.hello();
+
+        System.out.println("\nQuestion 13");
+        System.out.println("With method overloading, multiple methods can have the same name with different parameters and/or types \n" +
+                "instead of defining two methods that should do the same thing, it is better to overload one.\n");
+
+        System.out.println("Question 14");
+        Functions.numberType(1.55f,2.78f);
+
+        System.out.println("\nQuestion 15");
+       // Functions.getScanner();
+
+        System.out.println("\nQuestion 16");
+        int[] arr1 = {10, 30, 55, 100, 98, 75};
+        System.out.println(Functions.randomize(arr1));
+
+        System.out.println("\nQuestion 17");
+        Functions.strings("fullstack");
+
+        System.out.println("\nQuestion 18");
+        System.out.println("Local variables are defined within a function, and can only be used within the function within which they were defined.\n" +
+                "Global variables can be accessed from any function.");
+
+        System.out.println("\nQuestion 19");
+        Functions.printWord();
+        Functions.changeWord("javascript");
+        // הערך של המשתנה הגלובלי משתנה רק בפונקצייה עצמה
+
+        System.out.println("\nQuestion 20");
+        int[] arr2 = {2 ,6 ,7 ,8 ,34 ,16};
+        System.out.println(Arrays.toString(arr2));
+        Functions.multiplyZugi(arr2);
+        Functions.addOne(arr2);
+        Functions.setZero(arr2);
+        System.out.println(Arrays.toString(arr2)); // המערך המקורי השתנה
+
+        System.out.println("\nQuestion 21");
+        System.out.println(Functions.sumOfDigits(123456789));
+
+        System.out.println("\nQuestion 22");
+        String[] passwordArray = {"w" ,"2" ,"k" ,"5" ,"4" ,"6", "1","9"};
+        System.out.println(Arrays.toString(passwordArray));
+        System.out.println(Functions.password(passwordArray));
+
+        System.out.println("\nQuestion 23");
+        Functions.matrix1();
+
+        System.out.println("\nQuestion 24");
+        System.out.println("Recursion is the technique of making a function call itself. This technique provides a way to break complicated problems down into simple problems which are easier to solve.\n" +
+                "Every recursive function should have a stop condition, which is the condition where the function stops calling itself.\n");
+
+        System.out.println("Question 25");
+        System.out.println(Functions.atsertRecursion(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
